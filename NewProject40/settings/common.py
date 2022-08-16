732473907d8846ab3961a11f8116b2e9bb1741c6
Huzzy619 <blazingkrane@gmail.com>
2022-08-16 11:28:20 +0100

    changes static dirs

--- NewProject40/settings/common.py
+++ NewProject40/settings/common.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static' ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
