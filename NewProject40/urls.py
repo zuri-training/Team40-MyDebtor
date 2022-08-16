54397e3084f6f9243d09527a54c58984c3e443db
Huzzy619 <blazingkrane@gmail.com>
2022-08-16 10:56:26 +0100

    new

--- NewProject40/urls.py
+++ NewProject40/urls.py
@@ -25,6 +25,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('', include('core.urls')),
+
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
@@ -38,7 +40,6 @@ urlpatterns = [
 
     # linking info_hub urls
     path('', include ('info_hub.urls')),
-    path('', include('core.urls')),
 
 
     # path('biodata/<int:pk>', BioDataView.as_view()),
