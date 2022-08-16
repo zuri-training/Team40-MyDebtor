9a03a2cf40404657ce3365d517369c9cd378c3d7
Huzzy619 <blazingkrane@gmail.com>
2022-08-16 04:41:10 +0100

    settings

--- NewProject40/settings/prod.py
+++ NewProject40/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import * 
 
 ALLOWED_HOSTS = ['studebt4-prod.herokuapp.com']
@@ -7,3 +8,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+DATABASES = {
+    
+    'default': dj_database_url.config()
+}
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn NewProject40.wsgi
+web: gunicorn NewProject40.wsgi --log-file -
