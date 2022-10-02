72b7f525d45228c6d301bcea34e989b9d2a774e5
Huzzy619 <blazingkrane@gmail.com>
2022-10-02 22:07:42 +0100

    updated email settings

--- Studebt/prod.py
+++ Studebt/prod.py
@@ -22,3 +22,10 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ['CLOUD_API_KEY'],
     'API_SECRET': os.environ['CLOUD_API_SECRET'],
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = '617e747e2afc2c'
+EMAIL_HOST_PASSWORD = 'e99890b04db43b'
+EMAIL_PORT = '2525'
